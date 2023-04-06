b36c1d55fc7b8cee63cba96479d5a741cfbde4cb
Andre Meneses <andrelucas.meneses01@gmail.com>
2023-04-06 17:34:28 -0300

    testAgoraVai

--- script/main.js
+++ script/main.js
@@ -1,20 +1,25 @@
-$('.owl-carousel').owlCarousel({
-  stagePadding: 1,
-  loop:true,
-  margin:1,
-  nav:true,
-  autoplay: true,
-  autoplayTimeout:3800,
-  smartSpeed:1500,
-  responsive:{
-      0:{
-          items:2
-      },
-      600:{
-          items:3
-      },
-      1000:{
-          items:4
-      }
-  }
-});
+
+
+
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        stagePadding: 1,
+        loop:true,
+        margin:1,
+        nav:true,
+        autoplay: true,
+        autoplayTimeout:3800,
+        smartSpeed:1500,
+        responsive:{
+            0:{
+                items:2
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:4
+            }
+        }
+      });
+})
